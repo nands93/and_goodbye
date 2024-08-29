0d165086b53a31a7f90b55c75c2eb881f00a2086
nands93 <femmarques93+github@gmail.com>
2024-08-28 21:21:34 -0300

    organization

--- and_goodbye.c
+++ and_goodbye.c
@@ -4,7 +4,9 @@ int	main(int argc, char **argv)
 {
 	t_game		game;
 	SDL_Event	e;
-	if (argc == 2) {
+	
+	if (argc == 2) 
+	{
 		if (SDL_Init(SDL_INIT_VIDEO) < 0)
 		{
 			printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
